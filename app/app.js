5d91cfe7fe66bf25382988dbad413c7dcc631616
Supachai Laparparat <supachailllpay@gmail.com>
2018-02-01 11:52:14 +0700

    Implement app file

---
+++ app/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+const api = require('./routes/api')
+const data = require('./routes/data')
+
+app.use('/api', api)
+app.use('/data', data)
+
+app.use((req, res, next) => {
+  if (/\.\w+$/.test(req.path)) return next()
+  res.sendFile(path.resolve('views/index.html'))
+})
+
+app.use((req, res, next) => {
+  let err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send(err.message)
+})
+
+module.exports = app
