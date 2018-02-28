c50c2d26180b7754a1996d2b04e02c04df39ddc9
Supachai Laparparat <supachailllpay@gmail.com>
2018-02-28 10:24:44 +0700

    Update app component
    Combine all layout components together

--- editor/src/app.vue
+++ editor/src/app.vue
@@ -1,3 +1,60 @@
 <template>
-  <h1>Hello Vue.js</h1>
+  <div>
+    <layout>
+      <toolbar slot='toolbar' @save='save'></toolbar>
+      <left-pane slot='left-pane'></left-pane>
+      <right-pane slot='right-pane'></right-pane>
+    </layout>
+
+    <spinner :hidden='!loading'></spinner>
+  </div>
 </template>
+
+<script>
+  import Spinner from '@/components/spinner'
+  import Layout from '@/components/layout'
+  import Toolbar from '@/components/toolbar'
+  import LeftPane from '@/components/left-pane'
+  import RightPane from '@/components/right-pane'
+
+  export default {
+    components: {
+      'spinner': Spinner,
+      'layout': Layout,
+      'toolbar': Toolbar,
+      'left-pane': LeftPane,
+      'right-pane': RightPane
+    },
+
+    data: () => ({
+      loading: false
+    }),
+
+    methods: {
+      request (action) {
+        this.loading = true
+        action().then(() => {
+          setTimeout(() => {
+            this.loading = false
+          }, 500)
+        })
+      },
+      save () {
+        this.request(() => (
+          this.$store.dispatch('push')
+        ))
+      }
+    },
+
+    mounted () {
+      this.request(() => (
+        this.$store.dispatch('pull')
+      ))
+    }
+  }
+</script>
+
+<style lang='scss'>
+  @import '~@/styles/variables';
+  @import '~@/styles/base';
+</style>
