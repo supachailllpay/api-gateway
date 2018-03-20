e90738a8f89222e1275c06eb0e96ebaf55c34461
Supachai Laparparat <supachailllpay@gmail.com>
2018-03-20 13:19:31 +0700

    Update api route

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,26 +1,64 @@
 const express = require('express')
+const request = require('request')
+const vm = require('vm')
+const debug = require('debug')
 const database = require('../database')
+
 const router = express.Router()
+const log = debug('app:api')
+
+const makeGenerator = function * (array) {
+  let index = 0
+  while (index < array.length) {
+    yield array[index]
+    index += 1
+  }
+}
+
+const testRoute = function (route, req) {
+  let { path, method } = route
+  let pathExp = new RegExp(`^${path}$`)
+  let methodExp = new RegExp(method, 'i')
+  return pathExp.test(req.path) && methodExp.test(req.method)
+}
+
+const useRoute = function (route, req, res, next) {
+  let { type, response } = route
+  if (testRoute(route, req)) {
+    switch (type) {
+      case 'json':
+        res.json(response)
+        return
+      case 'proxy':
+        req.pipe(request(response)).pipe(res)
+        return
+      case 'function':
+        let sandbox = { req, res, next }
+        vm.createContext(sandbox)
+        vm.runInContext(response, sandbox)
+        return
+    }
+  }
+  next()
+}
 
 router.use((req, res, next) => {
-  let path = req.path
-  let method = req.method
+  database.get().then(data => {
+    let generator = makeGenerator(data.routes)
+    ;(function next () {
+      let route = generator.next().value
+      if (route) {
+        log(route.method, route.path)
+        useRoute(route, req, res, next)
+      }
+    }())
+  }).catch(next)
+})
 
-  let data = database.get('data')
-  let routes = data.routes
-  let found = false
-
-  routes.forEach(route => {
-    if (found) return
-    let pathExp = new RegExp(`^${route.path}$`)
-    let methodExp = new RegExp(route.method, 'i')
-    if (pathExp.test(path) && methodExp.test(method)) {
-      res.json(route.response)
-      found = true
-    }
-  })
-
-  if (!found) next()
+router.use((err, req, res, next) => {
+  log(err)
+  res.status(err.status || 500)
+  res.json({ ok: false, message: err.message })
 })
 
 module.exports = router
