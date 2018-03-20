9ddbd267c86e9d1b1320862f1201e2f2dc4f69b6
Supachai Laparparat <supachailllpay@gmail.com>
2018-03-20 12:49:46 +0700

    Update data route

--- app/routes/data.js
+++ app/routes/data.js
@@ -1,16 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const debug = require('debug')
 const database = require('../database')
+
 const router = express.Router()
+const log = debug('app:data')
+
+router.use(bodyParser.json())
 
 router.get('/', (req, res, next) => {
-  let data = database.get()
-  res.json(data)
+  database.get().then(data => {
+    res.json({ ok: true, data })
+  }).catch(next)
 })
 
-router.post('/', (req, res, next) => {
-  let data = req.body.data
-  database.set(data)
-  res.json({ ok: true })
+router.put('/', (req, res, next) => {
+  database.set(req.body.data).then(() => {
+    res.json({ ok: true })
+  }).catch(next)
+})
+
+router.use((err, req, res, next) => {
+  log(err)
+  res.status(err.status || 500)
+  res.json({ ok: false, message: err.message })
 })
 
 module.exports = router
