432909359a9bbe99efe4909522a6643c71959bf2
Supachai Laparparat <supachailllpay@gmail.com>
2018-03-24 16:21:01 +0700

    Update data store

--- editor/src/store/data.js
+++ editor/src/store/data.js
@@ -1,15 +1,14 @@
 const state = {
   info: {},
-  routes: [],
-  selectedRoute: {}
+  routes: []
 }
 
 const actions = {
   pull (context) {
-    window.state = state
     return window.fetch('/data')
       .then(response => response.json())
-      .then(data => {
+      .then(body => {
+        let data = body.data
         context.commit('setInfo', data.info || {})
         context.commit('setRoutes', data.routes || [])
       })
@@ -20,17 +19,31 @@ const actions = {
     let routes = context.state.routes
     let data = { info, routes }
     return window.fetch('/data', {
-      method: 'post',
+      method: 'put',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ data })
     })
   },
 
-  setRoutes (context, routes) {
-    context.commit('setRoutes', routes)
+  addRoute (context, payload) {
+    let route = payload.route
+    let routes = context.state.routes
+    let newRoutes = routes.concat(route)
+    context.commit('setRoutes', newRoutes)
   },
 
-  selectRoute (context, route) {
+  moveRoute (context, payload) {
+    let fromIndex = payload.fromIndex
+    let toIndex = payload.toIndex
+    let routes = context.state.routes
+    let newRoutes = routes.slice()
+    let route = newRoutes.splice(fromIndex, 1)[0]
+    newRoutes.splice(toIndex, 0, route)
+    context.commit('setRoutes', newRoutes)
+  },
+
+  selectRoute (context, payload) {
+    let route = payload.route
     context.commit('setSelectedRoute', route)
   }
 }
