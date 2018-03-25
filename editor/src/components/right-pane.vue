0ca79bbad6158a21ea14b9e3f2fed3dfaf4052b4
Supachai Laparparat <supachailllpay@gmail.com>
2018-03-25 16:14:10 +0700

    Update right-pane component

--- editor/src/components/right-pane.vue
+++ editor/src/components/right-pane.vue
@@ -1,17 +1,48 @@
 <template>
-  <div class='container'>
-    <div class='route'>
-      <input class='route-path' placeholder='Path' v-model='route.path'>
-      <input class='route-method' placeholder='Method' v-model='route.method'>
-    </div>
+  <div class='right-pane'>
+    <template v-if='route'>
+      <div class='roe'>
+        <input class='roe-p' placeholder='Path' v-model='route.path'>
+        <element-select class='roe-m' right
+          label='Method'
+          v-model='route.method'
+          :options='[
+            { label: "any", value: "*" },
+            { label: "get", value: "get" },
+            { label: "post", value: "post" },
+            { label: "put", value: "put" },
+            { label: "patch", value: "patch" },
+            { label: "delete", value: "delete" }
+          ]'>
+        </element-select>
+      </div>
 
-    <div class='editor'>
-      <textarea ref='textarea'></textarea>
-    </div>
+      <div class='edt'>
+        <div class='edt-h'>
+          <element-select
+            label='Type'
+            v-model='route.type'
+            :options='[
+              { label: "json", value: "json" },
+              { label: "proxy", value: "proxy" },
+              { label: "function", value: "function" }
+            ]'>
+          </element-select>
+        </div>
+        <div class='edt-t'>
+          <textarea ref='textarea'></textarea>
+        </div>
+      </div>
+    </template>
+
+    <template v-else>
+      <div class='ept'>ヽ(´▽｀)ノ</div>
+    </template>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
   import CodeMirror from 'codemirror'
   import 'codemirror/mode/javascript/javascript'
 
@@ -41,16 +72,20 @@
 
     watch: {
       route (route) {
-        let text = route.response || ''
-        this.editor.setValue(text)
+        if (!route) {
+          this.editor = null
+          return
+        }
+        Vue.nextTick(() => {
+          if (!this.editor) {
+            this.editor = initCodeMirror(this.$refs.textarea)
+            this.editor.on('change', () => {
+              this.route.response = this.editor.getValue()
+            })
+          }
+          this.editor.setValue(route.response || '')
+        })
       }
-    },
-
-    mounted () {
-      this.editor = initCodeMirror(this.$refs.textarea)
-      this.editor.on('change', () => {
-        this.route.response = this.editor.getValue()
-      })
     }
   }
 </script>
@@ -59,31 +94,49 @@
   @import '~@/styles/variables';
   @import '~@/styles/mixins';
 
-  .container {
+  .right-pane {
     padding: 24px;
   }
 
-  .route {
-    @include card;
+  .roe {
     display: flex;
+    padding: 16px;
+    background-color: $color-white;
+    border-radius: 4px;
   }
 
-  .route-path {
+  .roe-p {
     flex-grow: 1;
     min-width: 0;
     padding: 0;
   }
 
-  .route-method {
+  .roe-m {
     width: 60px;
     min-width: 0;
     padding: 0;
     margin-left: 16px;
   }
 
-  .editor {
-    @include card;
+  .edt {
     margin-top: 24px;
+    background-color: $color-white;
+    border-radius: 4px;
+  }
+
+  .edt-h {
+    padding: 16px;
+    border-bottom: 1px dashed $color-divider;
+  }
+
+  .edt-t {
+    padding: 16px;
+  }
+
+  .ept {
+    color: $color-divider;
+    font-size: 56px;
+    text-align: center;
   }
 </style>
 
