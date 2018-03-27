232e9eb4a27194f0975396e41290a752cdb47221
Supachai Laparparat <supachailllpay@gmail.com>
2018-03-28 01:21:57 +0700

    0.1.0

--- editor/src/components/toolbar.vue
+++ editor/src/components/toolbar.vue
@@ -2,7 +2,7 @@
   <div class='toolbar'>
     <div class='tle'>
       <a href='/'>{{ info.name || 'api-gateway' }}</a>
-      <span>v{{ info.version || '0.0.0' }}</span>
+      <span>v{{ info.version || '0.1.0' }}</span>
     </div>
 
     <div class='icn' @click='save'>save</div>
